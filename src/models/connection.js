75810eef897a54b5737b6d4f7b5e0f800405113b
Lucas Medeiros <92602681+Qluks@users.noreply.github.com>
2023-05-22 23:04:11 -0300

    Update connection.js

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB
+  host: "containers-us-west-207.railway.app",
+  user: "root",
+  password: "2dEMuDvizuqrzY48nVSu",
+  database: "railway"
 });
 
 module.exports = connection;
